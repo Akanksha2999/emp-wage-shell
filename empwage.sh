5d58d6927d090c41cf0e18d5f969ebf68dab813c
Akanksha2999 <>
2022-05-05 21:26:12 +0530

    program modified

--- empwage.sh
+++ empwage.sh
@@ -3,13 +3,16 @@
 echo "welcome to employee wage computation"
 
 isFullTime=1;
+isPartTime=2;
 empWagePerHour=20;
 
-empCheck=$((RANDOM%2));
+empCheck=$((RANDOM%3));
 
 case $empCheck in
 	$isFullTime)
 		empHrs=8;;
+	$isPartTime)
+		empHrs=4;;
 	*)
 		empHrs=0;;
 
